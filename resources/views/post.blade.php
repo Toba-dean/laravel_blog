1c9e887dcbcb3b4b538ed61d4b2e3c0fd744c7dd
Toba-dean <tobaogundimu@gmail.com>
2022-11-13 08:39:28 +0000

    added the components layout view

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-
-  <link rel="stylesheet" href="/app.css">
-  <title>My First Post</title>
-</head>
-<body>
+<x-layout>
+  <x-slot:title>
+    {{ $post->title }}
+  </x-slot>
   
   <article>
-
+  
     <h1>{{ $post->title }}</h1>
     {!! $post->body !!}
-
+  
   </article>
-
+  
   <a href="/" style="background-color: rgb(205, 200, 200); padding: 15px 20px; border-radius: 10px; display: inline-block; font-size: 18px">Go Home</a>
-
-</body>
-</html>
+</x-layout>
+  
