4d71c5b4b827df1aa4ad5689ef29dca92eb7d396
Toba-dean <tobaogundimu@gmail.com>
2022-11-15 13:29:05 +0000

    started adding the eloquent model for the posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,71 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-
-  public $date;
-  public $title;
-  public $excerpt;
-  public $slug;
-  public $body;
-
-  public function __construct($date, $title, $body, $excerpt, $slug)
-  {
-    $this->date = $date;
-    $this->title = $title;
-    $this->excerpt = $excerpt;
-    $this->body = $body;
-    $this->slug = $slug;
-  }
-
-  public static function all()
-  {
-    return cache()->rememberForever("post.all", function () {
-      return collect(File::files(resource_path("posts/")))
-        ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-        ->map(
-          fn ($document) => new Post(
-            $document->date,
-            $document->title,
-            $document->body(),
-            $document->excerpt,
-            $document->slug
-          )
-        )
-        ->sortByDesc('date');
-    });
-  }
-
-  // public static function find($slug)
-  // {
-  //   $path = resource_path("posts/{$slug}.html");
-  //   if (!file_exists($path)) {
-  //     throw new ModelNotFoundException();
-  //   }
-
-  //   return cache()->remember(
-  //     "post.{slug}",
-  //     60,
-  //     fn () => file_get_contents($path)
-  //   );
-  // }
-
-  static public function find($slug)
-  {
-    return static::all()->firstWhere("slug", $slug);
-  }
-
-  static public function findorfail($slug)
-  {
-    $post = static::find($slug);
-    if ($post === null) {
-      throw new ModelNotFoundException();
-    }
-    return $post;
-  }
+    use HasFactory;
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,9 @@
   <article>
   
     <h1>{{ $post->title }}</h1>
-    {!! $post->body !!}
+    <p>
+      {{ $post->body }}
+    </p>
   
   </article>
   
