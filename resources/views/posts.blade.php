d4b3e557de02a2135c7c7bc380daf3b58aa814d9
Toba-dean <tobaogundimu@gmail.com>
2022-11-20 09:52:15 +0000

    adding the html layout to the home view

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-  @foreach($posts as $post)
+  {{-- @foreach($posts as $post)
     <article>
 
       <h1>
@@ -22,5 +22,22 @@
       </div>
 
     </article>
-  @endforeach
+  @endforeach --}}
+
+  @include('_post-header')
+
+  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-featured-card />
+
+    <div class="lg:grid lg:grid-cols-2">
+      <x-post-card />
+      <x-post-card />
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+      <x-post-card />
+      <x-post-card />
+      <x-post-card />
+    </div>
+  </main>
 </x-layout>
