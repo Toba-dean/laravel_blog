2a4ffdfed367683664314f16895a64d0fc7781bf
Toba-dean <tobaogundimu@gmail.com>
2022-11-21 11:16:26 +0000

    made the post page dynamic with eloquent components

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -27,17 +27,13 @@
   @include('_post-header')
 
   <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    <x-post-featured-card />
+    @if ($posts->count())  
+      <x-post-grid :posts="$posts" />
 
-    <div class="lg:grid lg:grid-cols-2">
-      <x-post-card />
-      <x-post-card />
-    </div>
+      @else
+        <p class='text-center'>No Post Yet. Please check back later</p>
+    @endif
 
-    <div class="lg:grid lg:grid-cols-3">
-      <x-post-card />
-      <x-post-card />
-      <x-post-card />
-    </div>
+
   </main>
 </x-layout>
